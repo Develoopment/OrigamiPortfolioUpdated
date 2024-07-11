31d503f2edbfc7fdf56896cc21527f16ef9786b9
Develoopment <88688638+Develoopment@users.noreply.github.com>
2024-07-11 18:23:27 -0500

    trying again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-    './public/index.html',
-  ],
   content: [
     "./index.html",
 
